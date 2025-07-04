aef218d8756ba818c6da93feae1eb4eeeadbf78f
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2025-07-04 15:21:29 -0700

    Suppress rogue error messages from `psub`

--- misc/tools/rclone-cat.sh
+++ misc/tools/rclone-cat.sh
@@ -8,6 +8,7 @@ fish_bin="$(command -v fish)" || default_cat
 rclone_bin="$(command -v rclone)" || default_cat
 input_data=$(</dev/stdin)
 {
+    exec -- 2>/dev/null # Suppress rogue error messages from the `psub` function.
     "${fish_bin}" -N -c '$argv[1] --config (psub) config show' "${rclone_bin}" <<< "${input_data}"
 } || {
     echo "${input_data}"
